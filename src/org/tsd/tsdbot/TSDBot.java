6fc3ac90ebeedf36be01a1db8fefd2dc586bdf57
Joe <schoolyd@teamschoolyd.org>
2014-02-20 01:53:49 -0800

    implementing hbo news sweeper

--- src/org/tsd/tsdbot/TSDBot.java
+++ src/org/tsd/tsdbot/TSDBot.java
@@ -263,7 +263,7 @@ public class TSDBot extends PircBot implements Runnable {
 
         while(true) {
             try {
-                wait(30 * 1000); // check every 2 minutes
+                wait(120 * 1000); // check every 2 minutes
             } catch (InterruptedException e) {
                 // something notified this thread, panic.blimp
             }
