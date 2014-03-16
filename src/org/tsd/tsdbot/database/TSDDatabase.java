1945f8ead87555e8f707e7922ba24ab09aab67de
Joe <schoolyd@teamschoolyd.org>
2014-03-16 10:33:54 -0700

    switching to just storing show info in db -- blocks and episodes stay in memory

--- src/org/tsd/tsdbot/database/TSDDatabase.java
+++ src/org/tsd/tsdbot/database/TSDDatabase.java
@@ -2,9 +2,13 @@ package org.tsd.tsdbot.database;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.tsd.tsdbot.TSDBotLauncher;
 import org.tsd.tsdbot.TomCruise;
 
+import java.io.*;
 import java.sql.*;
+import java.util.LinkedList;
+import java.util.Properties;
 
 public class TSDDatabase {
 
@@ -28,11 +32,8 @@ public class TSDDatabase {
         try {
             if(conn == null || conn.isClosed())
                 conn = DriverManager.getConnection(connectionString);
-
-            PreparedStatement ps = conn.prepareStatement(testQ);
-            ps.executeQuery();
+            try(PreparedStatement ps = conn.prepareStatement(testQ);ResultSet result = ps.executeQuery()) {}
         } catch (SQLException sqle) {
-
             System.err.println("db test query failed: " + sqle.getMessage());
             System.err.println("TRIED USING " + connectionString);
             return null;
@@ -87,36 +88,103 @@ public class TSDDatabase {
         }
     }
 
+    private void initTSDTVDB() throws SQLException, IOException {
 
-//    public static void ensureTomCruiseQuotes(Connection conn) {
-//
-//        String create = "create table if not exists TomCruiseQuotes ( id int auto_increment, quote text, primary key (id) )";
-//
-//        try (PreparedStatement ps = conn.prepareStatement(create)) {
-//            ps.executeUpdate();
-//
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//
-//    public static void ensureTomCruiseClips(Connection conn) {
-//
-//        String create = "create table if not exists TomCruiseClips ( id int auto_increment, clip text, primary key (id) )";
-//
-//        try (PreparedStatement ps = conn.prepareStatement(create)) {
-//            ps.executeUpdate();
-//
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//    }
+        // reload all blocks
+        String blocksTable = "TSDTV_BLOCK";
+        String dropBlocks = String.format("drop table if exists %s", blocksTable);
+        try(PreparedStatement ps = getConnection().prepareStatement(dropBlocks)) {
+            ps.execute();
+        }
+
+        String createBlocks = String.format("create table %s (" +
+                "id int auto_increment," +
+                "name varchar," +
+                "quartzString varchar," +
+                "primary key (id))", blocksTable);
+        try(PreparedStatement ps = getConnection().prepareStatement(createBlocks)) {
+            ps.executeUpdate();
+        }
+
+        // load new shows
+        String showsTable = "TSDTV_SHOW";
+        String createShows = String.format("create table if not exists %s (" +
+                "id int auto_increment," +
+                "name varchar," +
+                "currentEpisode int," +
+                "primary key (id))", showsTable);
+        try(PreparedStatement ps = getConnection().prepareStatement(createShows)) {
+            ps.executeUpdate();
+        }
+
+        Properties prop = new Properties();
+        InputStream fis = TSDBotLauncher.class.getResourceAsStream("/tsdbot.properties");
+        prop.load(fis);
+        String catalogPath = prop.getProperty("tsdtv.catalog");
+        File catalogDir = new File(catalogPath);
+        for(File f : catalogDir.listFiles()) {
+            if(f.isDirectory()) {
+                String q = String.format("select count(*) from %s where name = '%s'", showsTable, f.getName());
+                try(PreparedStatement ps = getConnection().prepareStatement(q) ; ResultSet result = ps.executeQuery()) {
+                    result.next();
+                    if(result.getInt(1) == 0) { // show does not exist in db, add it
+                        String insertShow = String.format(
+                                "insert into %s (name, currentEpisode) values ('%s',1)",
+                                showsTable,
+                                f.getName());
+                        try(PreparedStatement ps1 = getConnection().prepareCall(insertShow)) {
+                            ps.executeUpdate();
+                        }
+                    }
+                }
+            }
+        }
+
+        // reload all episodes
+        String episodesTable = "TSDTV_EPISODE";
+        String dropEpisodes = String.format("drop table if exists %s", episodesTable);
+        try(PreparedStatement ps = getConnection().prepareStatement(dropEpisodes)) {
+            ps.execute();
+        }
+
+        String createEpisodes = String.format("create table %s (" +
+                "blockId int," +
+                "showId int," +
+                "order int)" +
+                blocksTable);
+        try(PreparedStatement ps = getConnection().prepareStatement(createEpisodes)) {
+            ps.executeUpdate();
+        }
+
+        FileInputStream schedule = new FileInputStream(new File(prop.getProperty("tsdtv.schedule")));
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(schedule))) {
+            String line = null;
+            while((line = br.readLine()) != null) {
+                if(line.startsWith("BLOCK")) {
+                    String blockName = line.substring(line.indexOf("=") + 1);
+                    String quartzString = br.readLine();
+                    LinkedList<String> shows = new LinkedList<>();
+                    while(!(line = br.readLine()).equals("ENDBLOCK")) {
+                        shows.add(line);
+                    }
+
+                    String newBlock = String.format(
+                            "insert into %s (name, schedule) values ('%s', '%s')",
+                            blocksTable, blockName, quartzString);
+                    try(PreparedStatement ps = getConnection().prepareStatement(newBlock)) {
+                        ps.executeUpdate();
+                    }
+
+                }
+            }
+        }
+
+
+    }
 
     public boolean tableExists(String tableName) throws SQLException {
         String q = String.format("select count(*) from information_schema.tables where table_name = '%s'",tableName);
-        PreparedStatement ps = getConnection().prepareStatement(q);
-        try(ResultSet result = ps.executeQuery()) {
+        try(PreparedStatement ps = getConnection().prepareStatement(q) ; ResultSet result = ps.executeQuery()) {
             result.next();
             return result.getInt(1) > 0;
         }
