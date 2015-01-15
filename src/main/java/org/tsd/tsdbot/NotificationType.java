cec44a9062cd81967fd09c21922d3e6def4e348f
Joe <schoolyd@teamschoolyd.org>
2015-01-15 01:12:19 -0800

    removing unnecessary fields from NotificationType

--- src/main/java/org/tsd/tsdbot/NotificationType.java
+++ src/main/java/org/tsd/tsdbot/NotificationType.java
@@ -7,20 +7,18 @@ import org.tsd.tsdbot.rss.RssFeedManager;
  * Created by Joe on 1/13/2015.
  */
 public enum NotificationType {
-    HBO_FORUM("HBO Forum", ".hbof", HboForumManager.class),
-    HBO_NEWS("HBO News",   ".hbon", HboNewsManager.class),
-    DBO_FORUM("DBO Forum", ".dbof", DboForumManager.class),
-    DBO_NEWS("DBO News",   ".dbon", DboNewsManager.class),
-    TWITTER("Twitter",     ".tw",   TwitterManager.class),
-    RSS_ITEM("RSS",        ".rss", RssFeedManager.class);
+    HBO_FORUM("HBO Forum", ".hbof"),
+    HBO_NEWS("HBO News",   ".hbon"),
+    DBO_FORUM("DBO Forum", ".dbof"),
+    DBO_NEWS("DBO News",   ".dbon"),
+    TWITTER("Twitter",     ".tw"),
+    RSS_ITEM("RSS",        ".rss");
 
     private String displayString;
     private String commandPrefix;
-    private Class<? extends NotificationManager> managerMap;
 
-    NotificationType(String displayString, String commandPrefix, Class<? extends NotificationManager> managerMap) {
+    NotificationType(String displayString, String commandPrefix) {
         this.displayString = displayString;
-        this.managerMap = managerMap;
         this.commandPrefix = commandPrefix;
     }
 
@@ -30,10 +28,6 @@ public enum NotificationType {
 
     public String getDisplayString() {
         return displayString;
-    }
-
-    public Class<? extends NotificationManager> getManagerMap() {
-        return managerMap;
     }
 
     public static NotificationType fromCommand(String cmd) {
