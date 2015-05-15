b90c22ae0690c6075a9511e8c2478ea4c3d66281
Joe <schoolyd@teamschoolyd.org>
2015-05-14 23:37:41 -0700

    anti-schroogling tech

--- src/main/java/org/tsd/tsdbot/functions/HarveyParty.java
+++ src/main/java/org/tsd/tsdbot/functions/HarveyParty.java
@@ -1,16 +1,21 @@
 package org.tsd.tsdbot.functions;
 
 import com.google.inject.Inject;
-import org.apache.http.HttpResponse;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
+import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.tsd.tsdbot.Bot;
 import org.tsd.tsdbot.Function;
 
 import java.util.concurrent.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by Joe on 5/14/2015.
@@ -43,8 +48,28 @@ public class HarveyParty extends MainFunctionImpl {
         Future<Integer> future = executorService.submit(new Callable<Integer>(){
             @Override
             public Integer call() throws Exception {
+
                 Integer statusCode = -1;
+                HttpGet get = null;
+                HttpPost post = null;
                 try {
+
+                    get = new HttpGet("http://www.theharveyparty.com/music/");
+                    CloseableHttpResponse getResponse = (CloseableHttpResponse) httpClient.execute(get);
+                    String pageText = EntityUtils.toString(getResponse.getEntity());
+
+                    String token = null;
+                    Pattern tokenPattern = Pattern.compile(".*(token=\\d+).*", Pattern.DOTALL);
+                    Matcher tokenMatcher = tokenPattern.matcher(pageText);
+                    while(tokenMatcher.find()) {
+                        token = tokenMatcher.group(1);
+                    }
+
+                    if(StringUtils.isEmpty(token))
+                        throw new Exception("Could not parse token from page");
+
+                    EntityUtils.consumeQuietly(getResponse.getEntity());
+
                     StringBuilder requestText = new StringBuilder();
                     for (int i = 1; i < cmdParts.length; i++) {
                         if (i != 1)
@@ -53,19 +78,29 @@ public class HarveyParty extends MainFunctionImpl {
                     }
 
                     String requester = "TSDIRC " + sender;
-                    String content = String.format("requester=%s&song=%s", requester, requestText.toString());
+                    String content = String.format("requester=%s&song=%s&token=%s",
+                            requester,
+                            requestText.toString(),
+                            token);
 
-                    HttpPost post = new HttpPost("http://www.theharveyparty.com/music/sendSong.php");
+                    post = new HttpPost("http://www.theharveyparty.com/music/sendSong.php");
                     post.setEntity(new StringEntity(content));
                     post.addHeader("Content-type", "application/x-www-form-urlencoded");
 
-                    HttpResponse response = httpClient.execute(post);
-                    statusCode = response.getStatusLine().getStatusCode();
+                    CloseableHttpResponse postResponse = (CloseableHttpResponse) httpClient.execute(post);
+                    statusCode = postResponse.getStatusLine().getStatusCode();
                     logger.info("HarveyParty response: {}", statusCode);
+
+                    EntityUtils.consumeQuietly(postResponse.getEntity());
 
                 } catch (Exception e) {
                     logger.error("Error sending song request", e);
                     bot.sendMessage(channel, "Error sending song request");
+                } finally {
+                    if(get != null)
+                        get.releaseConnection();
+                    if(post != null)
+                        post.releaseConnection();
                 }
 
                 return statusCode;
